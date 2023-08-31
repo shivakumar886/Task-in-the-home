4a70bfbdcde230bc2d2395059888739b14aa6991
shivakumar886 <shivukumar.xworkz@gmail.com>
2023-08-31 17:49:18 +0530

    31 aug assignment

---
+++ Application.java
@@ -0,0 +1,132 @@
+
+public class Application {
+   public static void main(String[] args){
+String pendrivename="hp v23464 GB";
+double pendrivecost=375.77;
+String pendrivecolor="Black";
+String pendrivestorage="64GB";
+String pendrivespeed="100mbps";
+
+System.out.println(pendrivename);
+System.out.println(pendrivecost);
+System.out.println(pendrivecolor);
+System.out.println(pendrivestorage);
+System.out.println(pendrivespeed);
+
+String dryfrutsname="Happilo";
+String dryfrutsweight="250 gm";
+String aboutdryfruts="it is zero colestrol";
+String aboutdryfrut="Zero-transfat";
+double dryfrutscost=271.55;
+
+System.out.println(dryfrutsname);
+System.out.println(dryfrutsweight);
+System.out.println(aboutdryfruts);
+System.out.println(aboutdryfrut);
+System.out.println(dryfrutscost);
+
+String laptopname="Dell";
+String laptopprice="22000";
+String laptopcolor="block";
+String laptopweight="3-4kg";
+String laptopbattery="5000";
+
+System.out.println(laptopname);
+System.out.println(laptopprice);
+System.out.println(laptopcolor);
+System.out.println(laptopweight);
+System.out.println(laptopbattery);
+
+String Bikename="MT15";
+String Bikecolor="red";
+String Bikeprice="22000";
+String Bikeenginecc="220";
+double Bikeweight=150.05;
+double Bikespeed=140.06;
+
+System.out.println(Bikename);
+System.out.println(Bikecolor);
+System.out.println(Bikeenginecc);
+System.out.println(Bikeweight);
+System.out.println(Bikeprice);
+System.out.println(Bikeprice);
+
+String Tvname="samsang";
+double Tvprice=150.06;
+double Tvweight=25.066;
+String color="block";
+short  tvsqfeat=30;
+long  tvipno=156432454;
+
+System.out.println(Tvname);
+System.out.println(Tvprice);
+System.out.println(Tvweight);
+System.out.println(color);
+System.out.println(tvsqfeat);
+System.out.println(tvipno);
+
+String Watercompaneyname="neslay";
+double waterprice=10.01;
+double waterweight=500.50;
+String Aboutwater="minerales adda.50gm";
+Short waterbotelspece=50;
+String waterbottelcolor="no color";
+
+System.out.println(Watercompaneyname);
+System.out.println(waterprice);
+System.out.println(Aboutwater);
+System.out.println(waterbotelspece);
+System.out.println(waterbottelcolor);
+System.out.println(Aboutwater);
+
+String phonename="one plus";
+double phoneprice=5000.50;
+short phonebotelspec=20;
+double phonebotelweight=250;
+long ipno=2551552;
+
+System.out.println(phonename);
+System.out.println(phoneprice);
+System.out.println(phonebotelspec);
+System.out.println(phonebotelweight);
+System.out.println(ipno);
+
+String bisketname="gooday";
+String bisketprice="10rs";
+double bisketweight=50;
+long bisketbarcodeno=15244252;
+short bisketmodel=20;
+
+System.out.println(bisketname);
+System.out.println(bisketprice);
+System.out.println(bisketweight);
+System.out.println(bisketbarcodeno);
+System.out.println(bisketmodel);
+
+String walletname="woodland";
+double weight=50.5;
+long walletbarcodeno=655566565;
+short walletpack=52;
+String price="15rs";
+
+System.out.println(walletname);
+System.out.println(weight);
+System.out.println(walletbarcodeno);
+System.out.println(walletpack);
+System.out.println(price);
+
+String gasstovename="TATA";
+String gasstoveweigth="25kg";
+long gastoveno=25151154;
+short shortno=25;
+double itsthick=2.5;
+
+System.out.println(gasstovename);
+System.out.println(gasstoveweigth);
+System.out.println(gastoveno);
+System.out.println(shortno);
+System.out.println(itsthick);
+
+
+    }
+}
