bfbaf537b5f945f1cba6a2f5bc636fb3446ac1a4
shivakumar886 <shivukumar.xworkz@gmail.com>
2023-09-01 17:40:01 +0530

    my first push

---
+++ Desktop/java/Simple.java
@@ -0,0 +1,10 @@
+class Simple{  
+    public static void main(String args[]){
+int mynumber=88;
+char my letter='D';
+char my text='Hello';
+System.out.println(mynumber);
+System.out.println(my letter);
+System.out.println(my text);
+    }  
+} 
